432cf66818807517afe1abf2b20cbf767abc76c5
Kevin Yang <kv7870@gmail.com>
2019-04-30 23:30:39 -0400

    d

--- Tower_of_Hanoi/class.cpp
+++ Tower_of_Hanoi/class.cpp
@@ -1,3 +1,4 @@
+//class.cpp
 #include "stdafx.h"
 #include "header.h"
 #include "class.h"
@@ -8,9 +9,9 @@ using namespace std;
 Peg::Peg(char c, int i) {
 	head = NULL;
 	pegID = c;
-	cx = i; 
+	cx = i;
 	size = 0;
-	
+
 	//top side
 	topY = 400;
 }
@@ -19,15 +20,15 @@ Peg::Peg(char c, int i) {
 Peg::Peg(char c, int pegX, int numDiscs) {
 	head = NULL;
 	pegID = c;
-	cx = pegX; 
-	size = 0; 
-	topY = 400; 
+	cx = pegX;
+	size = 0;
+	topY = 400;
 	//disc width 
-	int width = 50; 
+	int width = 50;
 	ALLEGRO_COLOR yellow = al_map_rgb(255, 255, 0);
 	ALLEGRO_COLOR blue = al_map_rgb(0, 255, 255);
 
-	
+
 	float x = 102.5, y = 400, r = 90;
 
 	for (int i = 0; i < numDiscs; i++) {
@@ -37,7 +38,7 @@ Peg::Peg(char c, int pegX, int numDiscs) {
 		temp->x = x;
 		temp->y = y;
 		temp->radius = r;
-		temp->colour = colour; 
+		temp->colour = colour;
 
 		push(temp);
 
@@ -48,12 +49,12 @@ Peg::Peg(char c, int pegX, int numDiscs) {
 
 void Peg::push(Node* newnode) {
 	newnode->next = head;
-	head = newnode; 
-	size++; 
+	head = newnode;
+	size++;
 	topY -= 20;
 }
 
-Node * Peg::pop(bool initial) {
+Node* Peg::pop(bool initial) {
 	Node* temp = head;
 	head = head->next;
 	topY += 20;
@@ -64,7 +65,7 @@ Node * Peg::pop(bool initial) {
 		temp = NULL;
 	}
 
-	return temp; 	
+	return temp;
 }
 
 Node* Peg::getHead() {
